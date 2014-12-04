8db174091d78adbd182118299d3aff99900fbeef
stevedubz <smw2@g.clemson.edu>
2014-12-04 16:12:18 -0500

    implemented commands and sleeping

--- UDPClient/UDPClient.c
+++ UDPClient/UDPClient.c
@@ -12,6 +12,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <unistd.h>
 
 #include "methods.h"
 
@@ -19,16 +20,12 @@ int main(int argc, char* argv[]) {
 	struct sockaddr_in serverAddress;	//Contains info for server address
 	char buffer[MAX];					//buffer for sending commands
 	FILE* cmds;							//file pointer to commands.txt
+	unsigned int sleeptime;
 	int sock;							//socket
 
 	if(argc > 2) {
 		err_n_die("Arguments: ./program <ip address>");
 	}
-
-	//read commands from text file.
-	cmds = fopen("commands.txt", "r");
-	fscanf(cmds,"%s", buffer);
-	fclose(cmds);
 
 	//create socket
 	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -41,9 +38,41 @@ int main(int argc, char* argv[]) {
 		err_n_die("inet_pton() failed\n");
 	}
 
-	if(sendto(sock, buffer, MAX, 0, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
-		printf("Error: %s\n", strerr(errno));
-		err_n_die("sendto() failed\n");
-	}
+	//read commands from text file.
+	cmds = fopen("commands.txt", "r");
+	while(getLines(&buffer, cmds)) {
+		//Get sleep time
+		if(strstr(buffer, "sleep") != NULL){
+			int i = 0;
+			for(i = 0; i < strlen(buffer); i++) {
+				if((int)buffer[i] < 10) {
+					sleeptime = (unsigned int)buffer[i];
+					sleep(sleeptime);
+					continue;
+				}
+			}
+		}
+		//default sleep time
+		else if(strstr(buffer, "sleep" != NULL)) {
+			sleeptime = 1;
+			sleep(sleeptime);
+			continue;
+		}
+		//send regular command
+		else {
+			if(sendto(sock, buffer, MAX, 0, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
+				printf("Error: %s\n", strerr(errno));
+				err_n_die("sendto() failed\n");
+			}
+		}
 
+	}
+	fclose(cmds);
+
+
+
+}
+
+ssize_t getLines(char **line, FILE* stream) {
+	return getdelim(line, '\n', stream);
 }
