a9162d264e9c97bea2042a933123f0e293803d4f
Clintos <clintostg@gmail.com>
2014-12-03 20:06:17 -0500

    Update response.h

--- response.h
+++ response.h
@@ -1,3 +1,10 @@
+/*
+I tested processResponse and it was working. Needs the buffer to be passed to it and the type to be passed to it (GPS, etc)
+The formatting or protocol that it uses is [response: 1]/r/n[type]/r/n[length]/r/n[data]
+
+Haven't tested sendResponse but I am pretty sure it works. If not it should be an easy fix.
+Needs the buffer, the udp client socket, and the client address.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
