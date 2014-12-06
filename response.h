e2c6bacb5d10495feed71decc11c7bf3cc6af825
Clintos <clintostg@gmail.com>
2014-12-06 18:30:08 -0500

    updated formatting

--- response.h
+++ response.h
@@ -82,7 +82,7 @@ char* processResponse(char *buffer, char *type)
 	}
 	databuf[k] = '\0';
 	static char resp[1024];
-	sprintf(resp, "1\r\n%s\r\n%d\r\n%s",type,length,databuf);
+	sprintf(resp, "%s\r\n%d\r\n%s",type,length,databuf);
 	free(databuf);
 	return resp;
 }
