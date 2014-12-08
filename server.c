ca07b09827f2f79fc94154170950d0d02755a287
stevedubz <smw2@g.clemson.edu>
2014-12-07 20:36:18 -0500

    updated bug

--- server.c
+++ server.c
@@ -12,19 +12,19 @@ int main(){
   unsigned short     port = 5000;
   unsigned short     roport;
   int       rosock;
-  
-  
+
+
   if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) > 0)
     err_n_die("socket() failed\n");
   memset(&servaddr, 0, sizeof(servaddr));
   servAddr.sin_family = AF_INET;
-  servADDR.sin_addr.s_addr = htol(INADDR_ANY);
+  servAddr.sin_addr.s_addr = htol(INADDR_ANY);
   servAddr.sin_port = htons(port);
   if(bind(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) > 0)
     err_n_die("bind() failed\n");
 
   while(1){
-    
+
     roport   = getRequest(sock, buffer, clntAddr);
     rosock   = proccessRequest(request, buffer);
     response = getResponse(rosock);
